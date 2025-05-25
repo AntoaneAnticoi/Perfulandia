d3b577a4ea543aef25ff0a12a8ee9f0919915343
antoane <an.anticoi@duocuc.cl>
2025-05-24 23:06:07 -0400

    Proyecto limpio Perfulandia

---
+++ src/test/java/com/perfulandia/Perfulandia/PerfulandiaApplicationTests.java
@@ -0,0 +1,21 @@
+<<<<<<<< HEAD:src/test/java/com/perfulandia/auth_service/AuthServiceApplicationTests.java
+package com.perfulandia.auth_service;
+========
+package com.perfulandia.Perfulandia;
+>>>>>>>> 00d790b (Cambios locales antes del pull):Perfulandia/src/test/java/com/perfulandia/Perfulandia/PerfulandiaApplicationTests.java
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+<<<<<<<< HEAD:src/test/java/com/perfulandia/auth_service/AuthServiceApplicationTests.java
+class AuthServiceApplicationTests {
+========
+class PerfulandiaApplicationTests {
+>>>>>>>> 00d790b (Cambios locales antes del pull):Perfulandia/src/test/java/com/perfulandia/Perfulandia/PerfulandiaApplicationTests.java
+
+	@Test
+	void contextLoads() {
+	}
+
+}
