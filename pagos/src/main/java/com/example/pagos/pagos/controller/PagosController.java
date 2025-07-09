8e09f6f7e6e7107e8aa572f99d46b1120d6aad64
antoane <an.anticoi@duocuc.cl>
2025-07-09 00:58:26 -0400

    Actualización

--- pagos/src/main/java/com/example/pagos/pagos/controller/PagosController.java
+++ pagos/src/main/java/com/example/pagos/pagos/controller/PagosController.java
@@ -2,10 +2,15 @@ package com.example.pagos.pagos.controller;
 
 import com.example.pagos.pagos.model.Pagos;
 import com.example.pagos.pagos.service.PagosService;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 @RequestMapping("/api/pagos")
@@ -18,25 +23,32 @@ public class PagosController {
         this.pagosService = pagosService;
     }
 
-
     @PostMapping
     public ResponseEntity<Pagos> realizarPago(@RequestBody Pagos pago) {
         Pagos nuevoPago = pagosService.realizarPago(pago);
         return ResponseEntity.ok(nuevoPago);
     }
 
-
     @GetMapping
-    public ResponseEntity<List<Pagos>> obtenerTodosLosPagos() {
-        return ResponseEntity.ok(pagosService.obtenerTodosLosPagos());
-    }
+    public ResponseEntity<CollectionModel<EntityModel<Pagos>>> obtenerTodosLosPagos() {
+        List<Pagos> pagos = pagosService.obtenerTodosLosPagos();
 
+        List<EntityModel<Pagos>> pagosModel = pagos.stream()
+                .map(pago -> EntityModel.of(pago,
+                        linkTo(methodOn(PagosController.class).obtenerPagoPorId(pago.getId())).withSelfRel(),
+                        linkTo(methodOn(PagosController.class).obtenerTodosLosPagos()).withRel("pagos")))
+                .collect(Collectors.toList());
+
+        CollectionModel<EntityModel<Pagos>> collectionModel = CollectionModel.of(pagosModel,
+                linkTo(methodOn(PagosController.class).obtenerTodosLosPagos()).withSelfRel());
+
+        return ResponseEntity.ok(collectionModel);
+    }
 
     @GetMapping("/usuario/{usuarioId}")
     public ResponseEntity<List<Pagos>> obtenerPagosPorUsuario(@PathVariable Long usuarioId) {
         return ResponseEntity.ok(pagosService.obtenerPagosPorUsuario(usuarioId));
     }
-
 
     @GetMapping("/{id}")
     public ResponseEntity<Pagos> obtenerPagoPorId(@PathVariable Long id) {
@@ -45,3 +57,4 @@ public class PagosController {
                 .orElse(ResponseEntity.notFound().build());
     }
 }
+
